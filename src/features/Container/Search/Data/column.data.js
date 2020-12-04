d072ec3061a7ca2d76130f65ea1bc7625f7a8d57
Samal <subhadatta.samal@iqvia.com>
2020-12-04 19:16:44 +0530

    Search UI Refinement Cont.

--- src/features/Container/Search/Data/column.data.js
+++ src/features/Container/Search/Data/column.data.js
@@ -1,41 +1,40 @@
-import CheckBox from 'apollo-react/components/Checkbox'
-import React from 'react';
+import CheckBox from "apollo-react/components/Checkbox";
+import React from "react";
 
 const CheckBoxCell = ({ row }) => {
-    return (
-        <div style={{ width: 92 }}>
-            <CheckBox />
-        </div>
-    );
+  return (
+    <div>
+      <CheckBox />
+    </div>
+  );
 };
 
 const columns = [
-    {
-        accessor: 'action',
-        customCell: CheckBoxCell,
-        width: 30,
-    },
-    {
-        header: 'Older Version',
-        accessor: 'olderVersion',
-    },
-    {
-        header: 'Source Document',
-        accessor: 'sourceDocument',
-    },
-    {
-        header: 'Upload Date',
-        accessor: 'uploadDate',
-    },
-    {
-        header: 'Version #',
-        accessor: 'version',
-    },
-    {
-        header: 'Document Status',
-        accessor: 'documentStatus',
-    },
-    
+  {
+    accessor: "action",
+    customCell: CheckBoxCell,
+    width: 30,
+  },
+  {
+    header: "Older Version",
+    accessor: "olderVersion",
+  },
+  {
+    header: "Source Document",
+    accessor: "sourceDocument",
+  },
+  {
+    header: "Upload Date",
+    accessor: "uploadDate",
+  },
+  {
+    header: "Version #",
+    accessor: "version",
+  },
+  {
+    header: "Document Status",
+    accessor: "documentStatus",
+  },
 ];
 
 export default columns;
