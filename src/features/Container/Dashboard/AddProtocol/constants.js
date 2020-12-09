e2b8b36539f2b687733e6ee7cba1d287b48a13e4
Asif <mohammadshadmaan.asif@iqvia.com>
2020-12-09 17:17:01 +0530

    Custom Dropdown with Add Functionality

--- src/features/Container/Dashboard/AddProtocol/constants.js
+++ src/features/Container/Dashboard/AddProtocol/constants.js
@@ -1,108 +1,108 @@
 export const initialFormValues = {
-    protocolNumber: "",
-    amendmentNumber: {
-      label: "",
-    },
-    projectID: "",
-    versionNumber: "",
-    sponsor: {
-      label: "",
-    },
-    sponsor1:[],
-    documentStatus: {
-        label: "",
-    },
-    indication: {
-        label: "",
-    },
-    moleculeDevice: "",
-    uploadFile:[]
-  }
-export const  initialFormErrorValues = {
-    protocolNumber: {
-        isRequired: false,
-        error: false,
-        errorMessage: " ",
-        type:'Textbox'
-      },
-      amendmentNumber: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type:'Dropdown'
-      },
-      projectID: {
-        isRequired: false,
-        error: false,
-        errorMessage: " ",
-        type:'Textbox'
-      },
-      versionNumber: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        regex: '^[0-9]*?\.[0-9]{0,2}$', // eslint-disable-line
-        type:'Textbox'
-      },
-      sponsor: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type:'Dropdown'
-      },
-      sponsor1: {
-        isRequired: false,
-        error: false,
-        errorMessage: " ",
-        type:'Dropdown'
-      },
-      documentStatus: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type:'Dropdown'
-      },
-      indication: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type:'Dropdown'
-      },
-      moleculeDevice: {
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type:'Textbox'
-      },
-      uploadFile:{
-        isRequired: true,
-        error: false,
-        errorMessage: " ",
-        type: 'File'
-      }
-}
+  protocolNumber: "",
+  amendmentNumber: {
+    label: "",
+  },
+  projectID: "",
+  versionNumber: "",
+  sponsor: {
+    label: "",
+  },
+  sponsor1: [],
+  documentStatus: {
+    label: "",
+  },
+  indication: {
+    label: "",
+  },
+  moleculeDevice: "",
+  uploadFile: [],
+};
+export const initialFormErrorValues = {
+  protocolNumber: {
+    isRequired: false,
+    error: false,
+    errorMessage: " ",
+    type: "Textbox",
+  },
+  amendmentNumber: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "Dropdown",
+  },
+  projectID: {
+    isRequired: false,
+    error: false,
+    errorMessage: " ",
+    type: "Textbox",
+  },
+  versionNumber: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    regex: "^[0-9]*?.[0-9]{0,2}$", // eslint-disable-line
+    type: "Textbox",
+  },
+  sponsor: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "CustomDropdown",
+  },
+  sponsor1: {
+    isRequired: false,
+    error: false,
+    errorMessage: " ",
+    type: "Dropdown",
+  },
+  documentStatus: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "Dropdown",
+  },
+  indication: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "CustomDropdown",
+  },
+  moleculeDevice: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "Textbox",
+  },
+  uploadFile: {
+    isRequired: true,
+    error: false,
+    errorMessage: " ",
+    type: "File",
+  },
+};
 
-export const documentStatusList= [
+export const documentStatusList = [
   {
     label: "Draft",
-    value: "draft"
+    value: "draft",
   },
   {
     label: "Approved Final",
-    value: "final"
-  }
+    value: "final",
+  },
 ];
 
-export const amendmentNumber=[
+export const amendmentNumber = [
   {
     label: "Y",
-    value: "Yes"
+    value: "Yes",
   },
   {
     label: "N",
-    value: "No"
-  }
+    value: "No",
+  },
 ];
-export const emptyAutoObj= {
-  label:''
-}
+export const emptyAutoObj = {
+  label: "",
+};
