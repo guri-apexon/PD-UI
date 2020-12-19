d95809300ef1a0384ad54ea509e6e24dcc3a029e
Samal <subhadatta.samal@iqvia.com>
2020-12-19 21:20:19 +0530

    Protocol overview unit test

--- src/features/Container/Protocols/ProtocolOverview.js
+++ src/features/Container/Protocols/ProtocolOverview.js
@@ -6,97 +6,98 @@ import _ from "lodash";
 const ProtocolOverview = ({ data }) => {
   return (
     <>
-      <div>
-        <div className="tab-card">
-          <Card style={{ padding: "10px 16px" }}>
-            <p className="card-header" data-testid="overview-test">
-              Overview Details
-            </p>
-            <div className="data-container">
-              <div className="column-6-12">
-                <div className="row-item">
-                  <label>Project ID or CRM #</label>
-                  <span>{data.ProjectId}</span>
-                </div>
-                <div className="row-item">
-                  <label>Sponser</label>
-                  <span>{data.Sponser}</span>
-                </div>
-                <div className="row-item">
-                  <label>Indication</label>
-                  <span>{data.Indication}</span>
-                </div>
-                <div className="row-item">
-                  <label>Molecule / Device</label>
-                  <span>{data.Molecule}</span>
-                </div>
+      <div className="tab-card">
+        <Card style={{ padding: "10px 16px" }}>
+          <p className="card-header" data-testid="overview-test">
+            Overview Details
+          </p>
+          <div className="data-container">
+            <div className="column-6-12">
+              <div className="row-item">
+                <label>Project ID or CRM #</label>
+                <span data-testid="project-value">{data.ProjectId}</span>
               </div>
-              <div className="column-6-12">
-                <div className="row-item">
-                  <label>Amendment</label>
-                  <span>{data.Amendment}</span>
-                </div>
-                <div className="row-item">
-                  <label>Version</label>
-                  <span>{data.VersionNumber}</span>
-                </div>
-                <div className="row-item">
-                  <label>Document Status</label>
-                  <span>{data.DocumentStatus}</span>
-                </div>
-                <div className="row-item">
-                  <label>Activity</label>
-                  <span>{data.Status}</span>
-                </div>
+              <div className="row-item">
+                <label>Sponser</label>
+                <span data-testid="sponser-value">{data.Sponser}</span>
+              </div>
+              <div className="row-item">
+                <label>Indication</label>
+                <span data-testid="indication-value">{data.Indication}</span>
+              </div>
+              <div className="row-item">
+                <label>Molecule / Device</label>
+                <span data-testid="molecule-value">{data.Molecule}</span>
               </div>
             </div>
-            <div className="protocol-title">
-              <div>Protocol Title</div>
-              <div>{data.ProtocolName}</div>
+            <div className="column-6-12">
+              <div className="row-item">
+                <label>Amendment</label>
+                <span data-testid="amendment-value">{data.Amendment}</span>
+              </div>
+              <div className="row-item">
+                <label>Version</label>
+                <span data-testid="version-value">{data.VersionNumber}</span>
+              </div>
+              <div className="row-item">
+                <label>Document Status</label>
+                <span data-testid="status-value">{data.DocumentStatus}</span>
+              </div>
+              <div className="row-item">
+                <label>Activity</label>
+                <span data-testid="activity-value">{data.Status}</span>
+              </div>
             </div>
-          </Card>
-        </div>
-        <Card style={{ padding: "10px 16px" }} className="chart-column">
-          <div className="">
-            <p className="chart-title">Digitized Confidence Interval</p>
-            {!isNaN(parseInt(data.confidenceInterval, 10)) ? (
-              <DonutChart
-                dropshadow
-                percent={parseInt(data.confidenceInterval, 10)}
-                subtitle="Label"
-                stroke="#0768fd"
-                tooltipTitle="$82,000"
-                tooltipSubtitle="Total sales in 2018"
-                style={{ height: "200px", width: "200px" }}
-              />
-            ) : (
-              <div style={{ display: "flex", height: 100 }}>
-                <p style={{ margin: "auto", fontSize: 12 }}>
-                  Information Not Available
-                </p>
-              </div>
-            )}
-            <p className="chart-title">Completeness of Digitization</p>
-            {!isNaN(parseInt(data.completenessDigitization, 10)) ? (
-              <DonutChart
-                dropshadow
-                percent={parseInt(data.completenessDigitization, 10)}
-                subtitle="Label"
-                // stroke="#0768fd"
-                tooltipTitle="$82,000"
-                tooltipSubtitle="Total sales in 2018"
-                style={{ height: "200px", width: "200px" }}
-              />
-            ) : (
-              <div style={{ display: "flex", height: 100 }}>
-                <p style={{ margin: "auto", fontSize: 12 }}>
-                  Information Not Available
-                </p>
-              </div>
-            )}
+          </div>
+          <div className="protocol-title">
+            <div>Protocol Title</div>
+            <div data-testid="title-value">{data.ProtocolName}</div>
           </div>
         </Card>
       </div>
+      <Card style={{ padding: "10px 16px" }} className="chart-column">
+        <div className="">
+          <p className="chart-title">Digitized Confidence Interval</p>
+          {!isNaN(parseInt(data.DigitizedConfidenceInterval, 10)) ? (
+            <DonutChart
+              dropshadow
+              percent={parseInt(data.DigitizedConfidenceInterval, 10)}
+              subtitle="Label"
+              stroke="#0768fd"
+              tooltipTitle=""
+              tooltipSubtitle=""
+              style={{ height: "200px", width: "200px" }}
+            />
+          ) : (
+            <div style={{ display: "flex", height: 100 }}>
+              <p
+                style={{ margin: "auto", fontSize: 12 }}
+                data-testid="no-data-confidense"
+              >
+                Information Not Available
+              </p>
+            </div>
+          )}
+          <p className="chart-title">Completeness of Digitization</p>
+          {!isNaN(parseInt(data.CompletenessOfDigitization, 10)) ? (
+            <DonutChart
+              dropshadow
+              percent={parseInt(data.CompletenessOfDigitization, 10)}
+              subtitle="Label"
+              // stroke="#0768fd"
+              tooltipTitle=""
+              tooltipSubtitle=""
+              style={{ height: "200px", width: "200px" }}
+            />
+          ) : (
+            <div style={{ display: "flex", height: 100 }}>
+              <p style={{ margin: "auto", fontSize: 12 }} data-testid="no-data-digitization">
+                Information Not Available
+              </p>
+            </div>
+          )}
+        </div>
+      </Card>
     </>
   );
 };
--- src/features/Container/Protocols/__test__/ProtocolOverview.test.js
+++ src/features/Container/Protocols/__test__/ProtocolOverview.test.js
@@ -1,233 +1,229 @@
-import React from "react";
-import {
-  render,
-  fireEvent,
-  act,
-  screen,
-  wait
-} from "../../../../test-utils/test-utils";
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { render, cleanup } from "@testing-library/react";
+import { fireEvent } from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
+
 import ProtocolOverview from "../ProtocolOverview";
+// import data from "./protocolSummary.json";
 
-import data from './protocolSummary.json'
+// console.log(data)
+afterEach(cleanup);
 
-import * as redux from "react-redux";
+const startingState = {
+  id: "abcd4601",
+  userId: "1021402",
+  fileName: "Protocol-2015-09-08-VER-V02-000001.PDF",
+  filePath: "/Protocol-2015-09-08-VER-V02-000001.pdf",
+  Protocol: "2015-01",
+  ProtocolName:
+    "ACURATE neo™ Aortic Bioprosthesis for Implantation using the ACURATE neo™ TA Transapical Delivery System in Patients with Severe Aortic Stenosis ",
+  ProjectId: 123,
+  Sponser: "Symetis S.A",
+  Indication: "Bone necrosis",
+  Molecule: "string",
+  Amendment: "string",
+  VersionNumber: 2,
+  DocumentStatus: "Final",
+  DraftVersion: 0,
+  errorCode: 0,
+  errorReason: "string",
+  Status: "Upload Complete",
+  phase: "III",
+  DigitizedConfidenceInterval: "string",
+  CompletenessOfDigitization: "string",
+  protocolTitle:
+    "ACURATE neo™ Aortic Bioprosthesis for Implantation using the ACURATE neo™ TA Transapical Delivery System in Patients with Severe Aortic Stenosis ",
+  studyStatus: "string",
+  sourceSystem: "string",
+  environment: "dev",
+  uploadDate: "2020-12-16T08:46:42.633000",
+  timeCreated: "2020-12-16T08:46:42.633000",
+  timeUpdated: "2020-12-16T08:46:42.633000",
+  userCreated: "1021402",
+  userModified: "string",
+  ApprovalDate: "2015-09-08T08:46:42.633000",
+  isActive: true,
+  iqvxmlpath: "string",
+  NctId: "0",
+};
+function reducer(startingState, action) {}
 
+function renderWithRedux(
+  Component,
+  { initialState, store = createStore(reducer, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{Component}</Provider>),
+  };
+}
 
 describe("Protocol Overview Testsuit", () => {
   test("Should render Protocol Overview Component", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    // container
-    let cardHeader = container.getByText(/Overview Details/);
-    expect(cardHeader).toHaveTextContent("Overview Details")
+    expect(getByText(/Overview Details/)).toHaveTextContent("Overview Details");
   });
+
   test("Should have project Id", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Project ID or CRM #/);
-    expect(cardHeader).toHaveTextContent("Project ID or CRM #")
-    
+    expect(getByText(/Project ID or CRM #/)).toHaveTextContent(
+      "Project ID or CRM #"
+    );
   });
-//   test("Should have project Id value", async () => {
-//     let container = render(
-//       <ProtocolOverview data={data} />,
-//       {
-//         initialState: {
-//           summary: {
-//             data:data,
-//             loading: false,
-//             success:true
-//           },
-//         },
-//       }
-//     );
-//     container = render(<ProtocolOverview data={data} />)
-//     let cardHeader = container.getByText(/Test Sample/);
-//     expect(cardHeader).toHaveTextContent("Test Sample")
-    
-//   });
-  
-  test("Should have Sponser", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+  it("Should have project Id with value", () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Sponser/);
-    expect(cardHeader).toHaveTextContent("Sponser")
-    
+    expect(getByTestId("project-value")).toHaveTextContent(123);
+  });
+
+  test("Should have Sponser", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    expect(getByText(/Sponser/)).toHaveTextContent("Sponser");
+  });
+  test("Should have Sponser with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    expect(getByTestId("sponser-value")).toHaveTextContent("Symetis S.A");
   });
 
   test("Should have Indication", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Indication/);
-    expect(cardHeader).toHaveTextContent("Indication")
-    
+    let cardHeader = getByText(/Indication/);
+    expect(cardHeader).toHaveTextContent("Indication");
+  });
+  test("Should have Indication with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("indication-value");
+    expect(cardHeader).toHaveTextContent("Bone necrosis");
   });
 
   test("Should have Amendment", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Amendment/);
-    expect(cardHeader).toHaveTextContent("Amendment")
-    
+    let cardHeader = getByText(/Amendment/);
+    expect(cardHeader).toHaveTextContent("Amendment");
+  });
+  test("Should have Amendment with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("amendment-value");
+    expect(cardHeader).toHaveTextContent("string");
   });
 
   test("Should have Version", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Version/);
-    expect(cardHeader).toHaveTextContent("Version")
-    
+    let cardHeader = getByText(/Version/);
+    expect(cardHeader).toHaveTextContent("Version");
+  });
+  test("Should have Version with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("version-value");
+    expect(cardHeader).toHaveTextContent(2);
   });
 
   test("Should have Document Status", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Document Status/);
-    expect(cardHeader).toHaveTextContent("Document Status")
-    
+    let cardHeader = getByText(/Document Status/);
+    expect(cardHeader).toHaveTextContent("Document Status");
+  });
+
+  test("Should have Document Status with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("status-value");
+    expect(cardHeader).toHaveTextContent("Final");
   });
 
   test("Should have Activity", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Activity/);
-    expect(cardHeader).toHaveTextContent("Activity")
-    
+    let cardHeader = getByText(/Activity/);
+    expect(cardHeader).toHaveTextContent("Activity");
+  });
+
+  test("Should have Activity with value", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("activity-value");
+    expect(cardHeader).toHaveTextContent("Upload Complete");
   });
 
   test("Should have Protocol Title", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Protocol Title/);
-    expect(cardHeader).toHaveTextContent("Protocol Title")
-    
+    let cardHeader = getByText(/Protocol Title/);
+    expect(cardHeader).toHaveTextContent("Protocol Title");
   });
 
   test("Should have Digitized Confidence Interval", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Digitized Confidence Interval/);
-    expect(cardHeader).toHaveTextContent("Digitized Confidence Interval")
-    
+    let cardHeader = getByText(/Digitized Confidence Interval/);
+    expect(cardHeader).toHaveTextContent("Digitized Confidence Interval");
+  });
+
+  test("Should show Information Not available for Confidence Interval, if data is not available", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("no-data-confidense");
+    expect(cardHeader).toHaveTextContent("Information Not Available");
+  });
+  test("Should show chart for Confidence Interval, if data is available", async () => {
+    startingState.DigitizedConfidenceInterval = "20";
+    const { getByTestId, getByText, queryByTestId } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = queryByTestId("no-data-confidense");
+    expect(cardHeader).toBeNull();
   });
 
   test("Should have Completeness of Digitization", async () => {
-    const container = render(
-      <ProtocolOverview data={data} />,
-      {
-        initialState: {
-          summary: {
-            data:data,
-            loading: false,
-            success:true
-          },
-        },
-      }
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
     );
-    let cardHeader = container.getByText(/Completeness of Digitization/);
-    expect(cardHeader).toHaveTextContent("Completeness of Digitization")
-    
+    let cardHeader = getByText(/Completeness of Digitization/);
+    expect(cardHeader).toHaveTextContent("Completeness of Digitization");
   });
-  
+  test("Should show Information Not available for Completeness of Digitization, if data is not available", async () => {
+    const { getByTestId, getByText } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = getByTestId("no-data-digitization");
+    expect(cardHeader).toHaveTextContent("Information Not Available");
+  });
+  test("Should show chart for Completeness of Digitization, if data is available", async () => {
+    startingState.CompletenessOfDigitization = "20";
+    const { getByTestId, getByText, queryByTestId } = render(
+      <ProtocolOverview data={startingState} />
+    );
+    let cardHeader = queryByTestId("no-data-digitization");
+    expect(cardHeader).toBeNull();
+  });
 });
