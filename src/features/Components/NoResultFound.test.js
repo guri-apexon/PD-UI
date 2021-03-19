68e3d6f8ba19144910a82e7cf8b6389980f30ad1
Asif <mohammadshadmaan.asif@iqvia.com>
2021-03-19 15:55:49 +0530

    removed counter.js and updated rootreducer, rootSaga, Unittest for NoResultFound Component

---
+++ src/features/Components/NoResultFound.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "../../test-utils/test-utils";
+
+import NoResultFound from "./NoResultFound";
+
+describe("PageNotFound", () => {
+  test("Render PageNot Found ", () => {
+    render(<NoResultFound />);
+  });
+});
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,4 +1,3 @@
-import counterReducer from "../features/counterSaga/counterSlice";
 import dashboardReducer from "../features/Container/Dashboard/dashboardSlice";
 import protocolReducer from "../features/Container/Protocols/protocolSlice";
 import searchReducer from "../features/Container/Search/searchSlice";
@@ -6,7 +5,6 @@ import userDetails from "./userDetails";
 import qcReducer from "../features/Container/QC/qcSlice";
 
 const rootReducer = {
-  counter: counterReducer,
   dashboard: dashboardReducer,
   protocol: protocolReducer,
   search: searchReducer,
