2e3806a0ffdfadba9acb5a03bd67a1c62f038942
Samal <subhadatta.samal@iqvia.com>
2021-05-19 19:13:56 +0530

    Alert Unit test cases

---
+++ src/features/Container/Navbar/__test__/data.js
@@ -0,0 +1,56 @@
+export const initialState = {
+  initialState: {
+    navbar: {
+      notifications: [
+        {
+          id: "1",
+          read: false,
+          header: "D8850C00003",
+          details: "Post-exposure Prophylaxis of COVID-19 in Adults",
+          timestamp: "2021-04-29T00:00:00",
+          protocolNumber: "D8850C00003",
+          aidocId: "45830060-0dcd-474f-a0e7-3974dd53b208",
+        },
+        {
+          id: "2",
+          read: false,
+          header: "201790",
+          details:
+            "Efficacy and safety of GSK3196165 versus placebo and tofacitinib in participants with moderately to severely active rheumatoid arthritis who have an inadequate response to methotrexate.",
+          timestamp: "2021-04-28T00:00:00",
+          protocolNumber: "201790",
+          aidocId: "0e915e8e-7424-48f9-a9bf-1fcda7be26a0",
+        },
+        {
+          id: "3",
+          read: true,
+          header: "Header",
+          details: "Lorem ipsum dolor sit ame",
+          timestamp: "2021-04-09T00:00:00",
+          protocolNumber: "D4191C00001",
+          aidocId: "3811a6fa1-36b4-4b8d-8c0d-2d7711ea8bfe",
+        },
+        {
+          id: "4",
+          read: false,
+          header: "Header",
+          details: "Lorem ipsum dolor sit ame",
+          timestamp: "2021-04-08T00:00:00",
+          protocolNumber: "D4191C00001",
+          aidocId: "4811a6fa1-36b4-4b8d-8c0d-2d7711ea8bfe",
+        },
+        {
+          id: "5",
+          read: true,
+          header: "Header",
+          details: "Lorem ipsum dolor sit ame",
+          timestamp: "2021-04-07T00:00:00",
+          protocolNumber: "D4191C00001",
+          aidocId: "5811a6fa1-36b4-4b8d-8c0d-2d7711ea8bfe",
+        },
+      ],
+      error: false,
+      loader: false,
+    },
+  },
+};
--- src/features/Container/Navbar/saga.js
+++ src/features/Container/Navbar/saga.js
@@ -2,12 +2,12 @@ import { takeEvery, all, call, put, select } from "redux-saga/effects";
 
 import { httpCall, BASE_URL_8000 } from "../../../utils/api";
 import { getNotification, setError } from "./navbarSlice";
-function* getState() {
-  const state = yield select();
-  const type = state.user.userDetail.user_type;
-  const id = state.user.userDetail.userId;
-  return { id: id.substring(1), type: type };
-}
+// function* getState() {
+//   const state = yield select();
+//   const type = state.user.userDetail.user_type;
+//   const id = state.user.userDetail.userId;
+//   return { id: id.substring(1), type: type };
+// }
 export function* navbarNotificationData() {
   const notificationUrl = `/notifications.json`;
   const notificationConfig = {
@@ -27,24 +27,24 @@ export function* navbarNotificationData() {
   }
 }
 
-export function* setRead(action) {
-  const notificationUrl = `${BASE_URL_8000}/notification?id=${action.payload}`;
-  const notificationConfig = {
-    url: notificationUrl,
-    method: "PUT",
-  };
-  try {
-    const notificationData = yield call(httpCall, notificationConfig);
+// export function* setRead(action) {
+//   const notificationUrl = `${BASE_URL_8000}/notification?id=${action.payload}`;
+//   const notificationConfig = {
+//     url: notificationUrl,
+//     method: "PUT",
+//   };
+//   try {
+//     const notificationData = yield call(httpCall, notificationConfig);
 
-    // if (notificationData.success) {
-    //   yield put(getNotification(notificationData.data));
-    // } else {
-    //   yield put(setError(notificationData.err.statusText));
-    // }
-  } catch (err) {
-    yield put(setError("Something Went Wrong"));
-  }
-}
+//     if (notificationData.success) {
+//       yield put(getNotification(notificationData.data));
+//     } else {
+//       yield put(setError(notificationData.err.statusText));
+//     }
+//   } catch (err) {
+//     yield put(setError("Something Went Wrong"));
+//   }
+// }
 
 export function* watchNavbar() {
   yield takeEvery("GET_NOTIFICATION_SAGA", navbarNotificationData);
