0832c50a5a6e02e1c483955627feb7735c381b68
u1072231 <sohan.khatawkar@iqvia.com>
2021-07-05 17:26:56 +0530

    QC protocol view API change

--- src/features/Container/QC/QCProtocolView/QCProtocolView.js
+++ src/features/Container/QC/QCProtocolView/QCProtocolView.js
@@ -18,7 +18,7 @@ function QCProtocolView({ protId, path, userType }) {
   useEffect(() => {
     dispatch({
       type: "GET_PROTOCOL_TOC_SAGA",
-      payload: { endPoint: "protocol_data/qc", id: protId },
+      payload: { endPoint: "protocol_data/", id: protId },
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
