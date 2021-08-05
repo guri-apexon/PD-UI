3459f26069f14fe9305b93b00042b86a3a0182e5
Samal <subhadatta.samal@iqvia.com>
2021-08-05 16:39:10 +0530

    Merge Comment changes

--- src/features/Container/Protocols/Documents.js
+++ src/features/Container/Protocols/Documents.js
@@ -59,7 +59,7 @@ const Documents = ({ handleChangeTab }) => {
           const fileName = splitArr[splitArr.length - 1];
 
           const file_path = `${UI_URL}/${fileName}`;
-          var a = document.createElement("A");
+          let a = document.createElement("A");
           a.href = file_path;
           a.download = file_path.substr(file_path.lastIndexOf("/") + 1);
           document.body.appendChild(a);
