bd5e65b0df1c6560b22131c131281264e96c0d27
Sohan Khatawkar <sohan.khatawkar@iqvia.com>
2021-09-17 08:57:52 +0000

    PD-2192 API integration changes

--- src/features/Container/Admin/_test_/AdminSaga.test.js
+++ src/features/Container/Admin/_test_/AdminSaga.test.js
@@ -1,5 +1,5 @@
 import { runSaga } from "redux-saga";
-import { usersFunction } from "../saga";
+import { usersFunction, deleteUser, updateUser } from "../saga";
 import * as api from "../../../../utils/api";
 
 describe("Admin Saga Unit Test", () => {
@@ -66,5 +66,151 @@ describe("Admin Saga Unit Test", () => {
       type: "",
     }).toPromise();
     expect(mockCallApi).toHaveBeenCalledTimes(1);
+  });
+
+  test("usersFunction Saga Failure", async () => {
+    const dispatchedActions = [];
+    const mockOutput = {
+      success: false,
+      err: {
+        statusText: "Error",
+      },
+    };
+    const mockCallApi = jest
+      .spyOn(api, "httpCall")
+      .mockImplementation(() => Promise.resolve(mockOutput));
+    const fakeStore = {
+      dispatch: (action) => dispatchedActions.push(action),
+      getState: () => ({
+        admin: {
+          users: [],
+          roles: [],
+          map: [],
+        },
+      }),
+    };
+    await runSaga(fakeStore, usersFunction, {
+      payload: {},
+      type: "",
+    }).toPromise();
+    expect(mockCallApi).toHaveBeenCalledTimes(1);
+  });
+
+  test("deleteUser Saga Success", async () => {
+    const dispatchedActions = [];
+    // const mockOutput = {
+    //   success: true,
+    //   data: [
+    //     {
+    //       username: "u1072231",
+    //       first_name: "Sohan",
+    //       last_name: "Khatawkar",
+    //       email: "sohan.khatawkar@iqvia.com",
+    //       country: "India",
+    //       date_of_registration: "2021-01-29T04:09:44.277000",
+    //       user_type: "normal",
+    //     },
+    //   ],
+    // };
+    // const mockCallApi = jest
+    //   .spyOn(api, "httpCall")
+    //   .mockImplementation(() => Promise.resolve(mockOutput));
+    const fakeStore = {
+      dispatch: (action) => dispatchedActions.push(action),
+      getState: () => ({
+        admin: {
+          users: [
+            {
+              username: "u1072231",
+              first_name: "Sohan",
+              last_name: "Khatawkar",
+              email: "sohan.khatawkar@iqvia.com",
+              country: "India",
+              date_of_registration: "2021-01-29T04:09:44.277000",
+              user_type: "normal",
+            },
+            {
+              username: "q1036048",
+              first_name: "Abhay",
+              last_name: "K",
+              email: "abhay.kumar2@quintiles.com",
+              country: "India",
+              date_of_registration: "2021-01-29T06:40:31.823000",
+              user_type: "QC2",
+            },
+          ],
+          roles: [],
+          map: [],
+        },
+      }),
+    };
+    await runSaga(fakeStore, deleteUser, {
+      payload: "q1036048",
+      type: "",
+    }).toPromise();
+    // expect(mockCallApi).toHaveBeenCalledTimes(1);
+  });
+
+  test("updateUser Saga Success", async () => {
+    const dispatchedActions = [];
+    // const mockOutput = {
+    //   success: true,
+    //   data: [
+    //     {
+    //       username: "u1072231",
+    //       first_name: "Sohan",
+    //       last_name: "Khatawkar",
+    //       email: "sohan.khatawkar@iqvia.com",
+    //       country: "India",
+    //       date_of_registration: "2021-01-29T04:09:44.277000",
+    //       user_type: "normal",
+    //     },
+    //   ],
+    // };
+    // const mockCallApi = jest
+    //   .spyOn(api, "httpCall")
+    //   .mockImplementation(() => Promise.resolve(mockOutput));
+    const fakeStore = {
+      dispatch: (action) => dispatchedActions.push(action),
+      getState: () => ({
+        admin: {
+          users: [
+            {
+              username: "u1072231",
+              first_name: "Sohan",
+              last_name: "Khatawkar",
+              email: "sohan.khatawkar@iqvia.com",
+              country: "India",
+              date_of_registration: "2021-01-29T04:09:44.277000",
+              user_type: "normal",
+            },
+            {
+              username: "q1036048",
+              first_name: "Abhay",
+              last_name: "K",
+              email: "abhay.kumar2@quintiles.com",
+              country: "India",
+              date_of_registration: "2021-01-29T06:40:31.823000",
+              user_type: "QC2",
+            },
+          ],
+          roles: [],
+          map: [],
+        },
+      }),
+    };
+    await runSaga(fakeStore, updateUser, {
+      payload: {
+        username: "u1072231",
+        first_name: "Sohan",
+        last_name: "Khatawkar",
+        email: "sohan.khatawkar@iqvia.com",
+        country: "India",
+        date_of_registration: "2021-01-29T04:09:44.277000",
+        user_type: "admin",
+      },
+      type: "",
+    }).toPromise();
+    // expect(mockCallApi).toHaveBeenCalledTimes(1);
   });
 });
