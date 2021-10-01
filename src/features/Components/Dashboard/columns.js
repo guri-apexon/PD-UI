54cf1b9705eb7718835cbf8730335beb2adc5ed5
Samal <subhadatta.samal@iqvia.com>
2021-10-01 12:10:17 +0530

    Testing CICD

--- src/features/Components/Dashboard/columns.js
+++ src/features/Components/Dashboard/columns.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import { covertMMDDYYYY } from "../../../utils/utilFunction";
 import Tooltip from "apollo-react/components/Tooltip";
