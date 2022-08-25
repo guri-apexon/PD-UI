060ecb34d32b436bb554d53e630d662edc30e24b
Samal <subhadatta.samal@iqvia.com>
2022-08-25 11:54:32 +0530

    pdf on docx

--- src/features/Container/Protocols/Tabs/NewProtocolView/DocRenderer/index.js
+++ src/features/Container/Protocols/Tabs/NewProtocolView/DocRenderer/index.js
@@ -1,43 +1,135 @@
-// import { useEffect } from "react";
-// // import FileViewer from "apollo-react/components/FileViewer";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fileStream } from "../../../store/slice";
-// import Loader from "apollo-react/components/Loader";
-// import { ActionTypes } from "../../../store/ActionTypes";
+import React, { useEffect, useRef, useState } from "react";
+import { Document, Page, pdfjs, Outline } from "react-pdf";
+import Pagination from "apollo-react/components/Pagination";
+import "./style.scss";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import ZoomOut from "../../../../../../assets/images/zoom-out.png";
+import ZoomIn from "../../../../../../assets/images/zoom-in.png";
+import menuIcon from "../../../../../../assets/images/list.png";
 
-// const file = "/POC/d5180c00025csp-FINAL-25Feb-clean.docx";
-// const type = "docx";
+import samplePDF from "./d5180c00025csp-FINAL-25Feb-clean.pdf";
+import { useDispatch, useSelector } from "react-redux";
+import { pdfPageNumber } from "../../../store/slice";
+import { ActionTypes } from "../../../store/ActionTypes";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const DocRenderer = ({ dfsPath, name }) => {
-  //   const dispatch = useDispatch();
-  //   const { loader, error } = useSelector(fileStream);
-  //   useEffect(() => {
-  //     dispatch({
-  //       type: ActionTypes.GET_FILE_STREAM,
-  //       payload: {
-  //         name,
-  //         dfsPath,
-  //         type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  //       },
-  //     });
-  //   }, [dfsPath, name]);
-  //   const onError = (e) => {
-  //     console.log(e, "error in file-viewer");
-  //   };
-  //   if (loader) {
-  //     return <Loader />;
-  //   }
-  //   if (error) {
-  //     return <h1>No data</h1>;
-  //   }
+const PDFRenderer = ({ name, dfsPath }) => {
+  const pdf_PageNumber = useSelector(pdfPageNumber);
+  // const { loader, error, data } = useSelector(fileStream);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [page, setPage] = useState(0);
+  const [showPagination, setShowPagination] = useState(true);
+  const [pdfScale, setPdfScale] = useState(1.0);
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const menuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: ActionTypes.GET_FILE_STREAM,
+  //     payload: { name, dfsPath, type: "application/pdf" },
+  //   });
+  // }, [dfsPath, name]);
+
+  useEffect(() => {
+    setPageNumber(pdf_PageNumber);
+  }, [pdf_PageNumber]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setDisplayMenu(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
+
+  useEffect(() => {
+    setPageNumber(page + 1);
+    // dispatch({ type: ActionTypes.PAGE_NUMBER, payload: page });
+  }, [page]);
+
+  useEffect(() => {
+    setPage(pageNumber - 1);
+  }, [pageNumber]);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function onItemClick({ pageNumber }) {
+    setPageNumber(pageNumber);
+    setDisplayMenu(false);
+    dispatch({ type: ActionTypes.PAGE_NUMBER, payload: pageNumber });
+  }
+
+  const handlePagination = (page) => {
+    setPage(page);
+    dispatch({ type: ActionTypes.PAGE_NUMBER, payload: page + 1 });
+  };
   return (
     <div className="pd-pdf-container">
-      <h4>Docx format is not supported.</h4>
-      {/* <FileViewer
-        src="https://www.informatica.com/content/dam/informatica-com/en/image/c03/c03-iqvia.jpg.thumbnail.300.169.png"
-        type="docx"
-      /> */}
+      <div onClick={() => setShowPagination(!showPagination)}>
+        <Document
+          file={samplePDF}
+          onLoadSuccess={onDocumentLoadSuccess}
+          onItemClick={onItemClick}
+        >
+          <div className="pdf-menu-container" ref={menuRef}>
+            <button
+              className="pdf-menu-button"
+              onClick={() => setDisplayMenu(!displayMenu)}
+            >
+              <img src={menuIcon} alt="menu" />
+            </button>
+            <div
+              className="pdf-menu"
+              style={{ display: displayMenu ? "flex" : "none" }}
+            >
+              {displayMenu && <Outline onItemClick={onItemClick} />}
+            </div>
+          </div>
+          <Page
+            pageNumber={pageNumber}
+            renderAnnotationLayer={true}
+            renderTextLayer={true}
+            scale={pdfScale}
+          />
+        </Document>
+      </div>
+      {showPagination && (
+        <div className="pd-pdf-navigation">
+          {numPages && (
+            <Pagination
+              count={numPages}
+              rowsPerPage={1}
+              page={page}
+              onChangePage={handlePagination}
+            />
+          )}
+          <div className="zoom-container">
+            <img
+              className="zoom-out"
+              src={ZoomOut}
+              alt="zoom-out"
+              onClick={() => setPdfScale(pdfScale - 0.2)}
+            />
+            <img
+              className="zoom-in"
+              src={ZoomIn}
+              alt="zoom-in"
+              onClick={() => setPdfScale(pdfScale + 0.2)}
+            />
+            {/* <button onClick={getSelectionText}>Copy</button> */}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-export default DocRenderer;
+export default PDFRenderer;
