e371a21611e0923e5ad6b357d68b77b016180724
Samal <subhadatta.samal@iqvia.com>
2022-09-07 17:36:01 +0530

    Css module correction

--- src/features/Components/GuidedTour/src/components/Carousel.js
+++ src/features/Components/GuidedTour/src/components/Carousel.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Navigation } from "swiper";
 
-import 'swiper/swiper.min.css';
-import 'swiper/modules/navigation/navigation.min.css';
-import 'swiper/modules/pagination/pagination.min.css';
-import 'swiper/modules/effect-fade/effect-fade.min.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-fade";
 
 const CarouselComp = ({ cards, closeTourCallback }) => {
+  const [reachedEnd, setReachedEnd] = useState(false);
 
-    const [reachedEnd, setReachedEnd] = useState(false);
+  const handleReachedEnd = () => {
+    setReachedEnd(true);
+  };
 
-    const handleReachedEnd = () => {
-        setReachedEnd(true);
-    }
-
-    return (
-        <>
-            <Swiper
-                spaceBetween={30}
-                effect={"fade"}
-                navigation={{
-                    nextEl: '.next',
-                    prevEl: 'swiper-button-disabled',
-
-                }}
-                modules={[EffectFade, Navigation]}
-                className="mySwiper"
-                onReachEnd={() => handleReachedEnd()}
-                onClick={() => {
-                    if (reachedEnd === true)
-                        closeTourCallback();
-                }}
-            >
-                {cards.map((card) => {
-                    return (<SwiperSlide className='next'>{card}</SwiperSlide>)
-                })}
-            </Swiper>
-        </>
-    )
-}
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={{
+          nextEl: ".next",
+          prevEl: "swiper-button-disabled",
+        }}
+        modules={[EffectFade, Navigation]}
+        className="mySwiper"
+        onReachEnd={() => handleReachedEnd()}
+        onClick={() => {
+          if (reachedEnd === true) closeTourCallback();
+        }}
+      >
+        {cards.map((card) => {
+          return <SwiperSlide className="next">{card}</SwiperSlide>;
+        })}
+      </Swiper>
+    </>
+  );
+};
 
 export default CarouselComp;
