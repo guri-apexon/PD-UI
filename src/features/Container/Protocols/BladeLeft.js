369ab35ecadff4a618d27a0d13ecfa9ad906f789
u1149919 <shubham.prakashkad@iqvia.com>
2022-12-14 14:50:28 +0530

    Minor Changes

--- src/features/Container/Protocols/BladeLeft.js
+++ src/features/Container/Protocols/BladeLeft.js
@@ -1,22 +1,24 @@
 import withStyles from '@material-ui/core/styles/withStyles';
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { neutral8 } from 'apollo-react/colors';
+import Tooltip from 'apollo-react/components/Tooltip';
 import PropTypes from 'prop-types';
 import Blade from 'apollo-react/components/Blade';
-import Button from 'apollo-react/components/Button';
 import Typography from 'apollo-react/components/Typography';
 import Accordion from 'apollo-react/components/Accordion';
 import AccordionDetails from 'apollo-react/components/AccordionDetails';
 import AccordionSummary from 'apollo-react/components/AccordionSummary';
-import ArrowRight from 'apollo-react-icons/ArrowRight';
-import Avatar from 'apollo-react/components/Avatar';
+import ArrowDown from 'apollo-react-icons/ArrowDown';
+import './LeftBlade.scss';
 
 const styles = {
-  content: {
+  blade: {
     color: neutral8,
     lineHeight: '24px',
     marginTop: '4.5%',
+    width: '200px',
   },
 };
 
@@ -31,56 +33,113 @@ function BladeLeft({ handlePageNo }) {
   const onChange = (e, expanded) => {
     setExpanded(expanded);
     console.log({ expanded });
+    console.log('Open', open);
   };
 
-  const onOpen = () => {
-    setOpen(true);
-    setExpanded(true);
+  const handleClick = (expanded) => {
+    setExpanded(expanded);
+    console.log('expandable', { expanded });
   };
 
   const data = [
-    { section: 'abc', pageNo: 2 },
-    { section: 'xyz', pageNo: 5 },
+    {
+      section:
+        'abcrtyuuhgfddfghgdvhtfgyfghjjyujnbvgyjbvghbvfghthgffyujjgggjjnbg',
+      pageNo: 2,
+      subsection: [{ sub_section: 'subsection1' }],
+    },
+    {
+      section: 'xyz1',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
+    {
+      section: 'xyz2',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
+    {
+      section: 'xyz3',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
+    {
+      section: 'xyz4',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
+    {
+      section: 'xyz5',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
+    {
+      section: 'xyz6',
+      pageNo: 5,
+      subsection: [
+        { sub_section: 'subsection1' },
+        { sub_section: 'subsection1' },
+      ],
+    },
   ];
 
   return (
     <div>
-      {/* <Avatar size="small" onClick={onOpen}>
-        <ArrowRight />
-      </Avatar> */}
-
-      <div>
+      <div style={{ backgroundColor: 'black' }}>
         <Blade
           onChange={onChange}
           open={open}
           expanded={expanded}
-          onClose={onClose}
+          // onClose={onClose}
           title="Navigation"
-          hasBackdrop
+          className="blade"
+          width={215}
+          marginTop={170}
         >
-          <div>
+          <div style={{ background: 'red' }}>
             {data.map((item, index) => {
               return (
                 <Accordion
                   key={React.key}
                   style={{ background: '#f8f9fb;', border: 'none' }}
                 >
-                  <AccordionSummary
-                    style={{
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                    }}
-                  >
-                    <Typography
-                      onClick={(e) => {
-                        handlePageNo(e, item.pageNo, index);
-                        console.log(item.pageNo);
-                      }}
-                    >
-                      {item.section}
-                    </Typography>
+                  <AccordionSummary expandIcon={<ArrowDown />}>
+                    <Tooltip title={item.section}>
+                      <Typography
+                        style={{
+                          whiteSpace: 'nowrap',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          width: '30px',
+                          marginTop: '3.5%',
+                        }}
+                        onClick={(e) => {
+                          // setExpanded(false);
+                          // setOpen(true);
+                          // onChange(e,);
+                          handlePageNo(e, item.pageNo, index);
+                        }}
+                      >
+                        {item.section}
+                      </Typography>
+                    </Tooltip>
                   </AccordionSummary>
+
                   <AccordionDetails>
                     <Typography>Subsection {index} </Typography>
                   </AccordionDetails>
