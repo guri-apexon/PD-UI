c52d51bcabe4491139856107d2de57d3f0e1e289
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-12-21 18:26:51 +0530

    ui fixes and eslint fixes

--- src/utils/api.js
+++ src/utils/api.js
@@ -185,8 +185,8 @@ let UIhost;
 /* eslint-enable */
 const environment = process.env.REACT_APP_ENV;
 if (environment === 'local') {
-  backendHost = 'http://127.0.0.1:8001';
-  // backendHost = 'https://dev-protocoldigitalization-api.work.iqvia.com';
+  backendHost = 'https://dev-protocoldigitalization-api.work.iqvia.com';
+  // backendHost = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8001';
   backendPostHost = 'https://dev-protocoldigitalization-ai.work.iqvia.com';
   UIhost = 'https://dev-protocoldigitalization-ui.work.iqvia.com';
   baseUrlSSO = 'https://dev-protocoldigitalization.work.iqvia.com/v1';
