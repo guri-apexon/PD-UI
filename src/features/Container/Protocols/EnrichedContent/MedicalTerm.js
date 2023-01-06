c519d55c6fc2a4af956f37052666d0dc9e300cec
Gurpreet Singh <gurpreet.singh@iqvia.com>
2023-01-06 16:33:17 +0530

    fixes for logic

--- src/features/Container/Protocols/EnrichedContent/MedicalTerm.js
+++ src/features/Container/Protocols/EnrichedContent/MedicalTerm.js
@@ -2,60 +2,86 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'apollo-react/components/Button';
 import Card from 'apollo-react/components/Card';
+import Modal from 'apollo-react/components/Modal';
 import Popper from 'apollo-react/components/Popper';
-import Typography from 'apollo-react/components/Typography';
+import TextField from 'apollo-react/components/TextField';
 import Pencil from 'apollo-react-icons/Pencil';
 import ArrowRight from 'apollo-react-icons/ArrowRight';
 import './MedicalTerm.scss';
 
-const clinicalTerms = [
+const clinicalTermsMockData = [
   {
     value: 'Synonims',
-    text: 'Synonims',
-    data: [{ value: 'Euismod' }, { value: 'Molestie' }, { value: 'Luptatum' }],
+    termLabel: 'Synonims',
+    data: [
+      { label: 'Euismod dsds dsds ds' },
+      { label: 'Molestie' },
+      { label: 'Luptatum' },
+    ],
   },
-  { value: 'Clinical Terms', text: 'Clinical Terms' },
-  { value: 'Ontology', text: 'Ontology' },
+  { value: 'Clinical Terms', termLabel: 'Clinical Terms' },
+  { value: 'Ontology', termLabel: 'Ontology' },
   {
     value: 'Prefered Term',
-    text: 'Preferred Term',
-    data: [{ value: 'Euismod' }, { value: 'Molestie' }, { value: 'Luptatum' }],
+    termLabel: 'Preferred Term',
+    data: [{ label: 'Euismod' }, { label: 'Molestie' }, { label: 'Luptatum' }],
   },
-  { value: 'Classification', text: 'Classification' },
+  { value: 'Classification', termLabel: 'Classification' },
 ];
 function MedicalTerm({ enrichedTarget }) {
+  const [clinicalTerms, setclinicalTerms] = useState(clinicalTermsMockData);
   const [anchorEl, setAnchorEl] = useState(null);
   const [SanchorEl, setSAnchorEl] = useState(null);
   const [selectedTerm, setSelectedTerm] = useState(null);
+  const [childTermValue, setChildTermValue] = useState(false);
+  const [newTermValue, setNewTermValue] = useState('');
 
   const childDataArr = () => {
-    return clinicalTerms.find((x) => x.text === selectedTerm)?.data;
+    return clinicalTerms.find((x) => x.termLabel === selectedTerm)?.data;
   };
-  const editItem = () => {
-    console.log('editItem');
+  const handleSave = () => {
+    if (!childTermValue || !selectedTerm) return false;
+    setclinicalTerms((prevState) =>
+      prevState.map((prev) => {
+        if (prev.termLabel === selectedTerm) {
+          return {
+            ...prev,
+            data: prev.data.map((x) =>
+              x.label === childTermValue ? { ...x, label: newTermValue } : x,
+            ),
+          };
+        }
+        return prev;
+      }),
+    );
+    setChildTermValue(null);
+    return true;
   };
   useEffect(() => {
     setAnchorEl(enrichedTarget || null);
     if (!enrichedTarget) setSelectedTerm(null);
   }, [enrichedTarget]);
+  useEffect(() => {
+    setNewTermValue(childTermValue);
+  }, [childTermValue]);
   return (
     <div className="enriched-menu-wrapper">
-      <Popper open={!!anchorEl} anchorEl={anchorEl}>
+      <Popper open={!!anchorEl} anchorEl={anchorEl} placement="bottom-start">
         <Card interactive className="main-popper">
           <div className="terms-list">
             {clinicalTerms.map((item) => {
-              const isActive = selectedTerm === item.text && item.data?.length;
+              const isActive =
+                selectedTerm === item.termLabel && item.data?.length;
               return (
-                <li key={item.value}>
+                <li key={item.termLabel}>
                   <Button
                     className="term-item"
                     onClick={(e) => {
-                      setSelectedTerm(item.text);
+                      setSelectedTerm(item.termLabel);
                       setSAnchorEl(!SanchorEl ? e.currentTarget : null);
                     }}
-                    value={item.value}
                   >
-                    {item.text}
+                    {item.termLabel}
                     {isActive && <ArrowRight />}
                   </Button>
                 </li>
@@ -74,10 +100,13 @@ function MedicalTerm({ enrichedTarget }) {
           <div className="terms-list">
             {childDataArr()?.map((item) => {
               return (
-                <li key={item.value}>
-                  <Button value={item.value} className="term-item">
-                    {item.value}
-                    <Pencil className="edit-Icon" onClick={editItem} />
+                <li key={item.label}>
+                  <Button value={item.label} className="term-item">
+                    <span className="sub-term-text">{item.label}</span>
+                    <Pencil
+                      className="edit-Icon"
+                      onClick={() => setChildTermValue(item.label)}
+                    />
                   </Button>
                 </li>
               );
@@ -85,6 +114,26 @@ function MedicalTerm({ enrichedTarget }) {
           </div>
         </Card>
       </Popper>
+      <Modal
+        disableBackdropClick
+        open={childTermValue}
+        variant="default"
+        onClose={() => setChildTermValue(null)}
+        title="Rename Clinical Term"
+        buttonProps={[
+          { size: 'small' },
+          { label: 'Rename', onClick: handleSave, size: 'small' },
+        ]}
+        id="renameTermsModal"
+      >
+        <TextField
+          size="small"
+          fullWidth
+          value={newTermValue}
+          onChange={(e) => setNewTermValue(e.target.value)}
+          placeholder="Enter clinical term name"
+        />
+      </Modal>
     </div>
   );
 }
