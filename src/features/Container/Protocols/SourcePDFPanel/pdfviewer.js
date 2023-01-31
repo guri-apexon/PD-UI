5ecec70727b899f593999f3e520e84d8676d6f1a
u1149919 <shubham.prakashkad@iqvia.com>
2023-01-31 20:05:20 +0530

    Minor Fixes

--- src/features/Container/Protocols/SourcePDFPanel/pdfviewer.js
+++ src/features/Container/Protocols/SourcePDFPanel/pdfviewer.js
@@ -34,21 +34,25 @@ function Pdf({ page, refs, pageRight, handlePaginationPage }) {
   }
 
   const addMouseMove = (e) => {
-    const { className } = e.target;
-    if (
-      className.toString().includes('Panel-handle') ||
-      className.toString().includes('Panel-handleContainer')
-    )
-      document.addEventListener('mousemove', changeScale, false);
+    if (e) {
+      const { className } = e.target;
+      if (
+        className.toString().includes('Panel-handle') ||
+        className.toString().includes('Panel-handleContainer')
+      )
+        document.addEventListener('mousemove', changeScale, false);
+    }
   };
 
   const removeMouseMove = (e) => {
-    const { className } = e.target;
-    if (
-      className.toString().includes('Panel-handle') ||
-      className.toString().includes('Panel-handleContainer')
-    )
-      document.removeEventListener('mousemove', changeScale, false);
+    if (e) {
+      const { className } = e.target;
+      if (
+        className.toString().includes('Panel-handle') ||
+        className.toString().includes('Panel-handleContainer')
+      )
+        document.removeEventListener('mousemove', changeScale, false);
+    }
   };
 
   useEffect(() => {
