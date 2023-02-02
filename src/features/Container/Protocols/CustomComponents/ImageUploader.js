c246b6bff0abc8d8357b966960ccde6eba540d94
Muraleedharan <anil.kumar3@iqvia.com>
2023-02-02 12:13:37 +0530

    image edit changes

--- src/features/Container/Protocols/CustomComponents/ImageUploader.js
+++ src/features/Container/Protocols/CustomComponents/ImageUploader.js
@@ -8,7 +8,10 @@ import './ImageUploader.scss';
 
 function ImageUploader({ lineID, content, edit }) {
   const [img, setImg] = useState(null);
+  const [imgBkp, setImgBkp] = useState(null);
   const [value, setValue] = useState([]);
+  const [isEdit, setIsEdit] = useState(true);
+  const [showEditBtn, setShowEditBtn] = useState(false);
   const { dispatchSectionEvent } = useContext(ProtocolContext);
 
   const handleDelete = () => {
@@ -21,6 +24,7 @@ function ImageUploader({ lineID, content, edit }) {
       content: img,
     };
     dispatchSectionEvent('CONTENT_UPDATE', obj);
+    setIsEdit(false);
   };
 
   const getBase64image = async (file) => {
@@ -46,44 +50,91 @@ function ImageUploader({ lineID, content, edit }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [content]);
 
+  const onImgEditBtnClick = () => {
+    setIsEdit(true);
+    setImgBkp(img);
+    setValue([]);
+  };
+
+  const handleCancel = () => {
+    setImg(imgBkp);
+    setImgBkp(null);
+    setIsEdit(false);
+  };
+
+  // eslint-disable-next-line no-nested-ternary
   return edit ? (
-    <div className="img-container">
-      <div className="btn-container">
-        <Button
-          variant="secondary"
-          size="small"
-          style={{ marginRight: 10 }}
-          onClick={handleDelete}
-        >
-          Delete
-        </Button>
+    isEdit ? (
+      <div className="img-container">
+        <div className="btn-container">
+          <Button
+            variant="secondary"
+            size="small"
+            style={{ marginRight: 10 }}
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
 
-        <Button
-          variant="primary"
-          size="small"
-          style={{ marginRight: 10 }}
-          onClick={handleSave}
-        >
-          Save
-        </Button>
+          <Button
+            variant="secondary"
+            size="small"
+            style={{ marginRight: 10 }}
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            variant="primary"
+            size="small"
+            style={{ marginRight: 10 }}
+            onClick={handleSave}
+            disabled={value.length === 0}
+          >
+            Save
+          </Button>
+        </div>
+
+        {content === '' && value.length === 0 ? (
+          <FileUpload
+            label=""
+            required
+            value={value}
+            maxItems={1}
+            onUpload={handleUpload}
+            onFileDelete={handleDelete}
+          />
+        ) : (
+          <img src={img} alt="img" className="richTextImg" />
+        )}
       </div>
-
-      {content === '' && value.length === 0 ? (
-        <FileUpload
-          label=""
-          required
-          value={value}
-          maxItems={1}
-          onUpload={handleUpload}
-          onFileDelete={handleDelete}
-        />
-      ) : (
+    ) : (
+      /* eslint-disable */
+      <div
+        className="img-container-read-mode"
+        onMouseOver={() => setShowEditBtn(true)}
+        onMouseLeave={() => setShowEditBtn(false)}
+      >
         <img src={img} alt="img" className="richTextImg" />
-      )}
-    </div>
+        {showEditBtn && (
+          <div className="img-edit-btn-container">
+            <Button
+              variant="primary"
+              size="small"
+              style={{ marginRight: 10 }}
+              onClick={onImgEditBtnClick}
+            >
+              Edit
+            </Button>
+          </div>
+        )}
+      </div>
+      /* eslint-enable */
+    )
   ) : (
     img && (
-      <div className="img-container">
+      <div className="img-container-read-mode">
         <img src={img} alt="img" className="richTextImg" />
       </div>
     )
