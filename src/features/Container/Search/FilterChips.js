42001852d863b4254f786897760a9db3de387e06
Keshri <Amit.Keshari@iqvia.com>
2023-02-09 14:21:32 +0530

    fixed sona issue for utilsFunction

--- src/features/Container/Search/FilterChips.js
+++ src/features/Container/Search/FilterChips.js
@@ -125,7 +125,7 @@ function FilterChip(props) {
     filters.qcStatus.length > 0 ||
     (filters.dateType.length > 0 &&
       filters.dateType[0].value !== 'approval_date') ||
-    (filters.dateSection.length > 0 && filters.dateSection[0].value?.filters.dateSection[0].value) ||
+    (filters.dateSection.length > 0 && filters.dateSection[0].value !== '0') ||
     (rangeDate.from && rangeDate.to)
   ) {
     return (
