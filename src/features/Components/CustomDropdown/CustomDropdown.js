c83d4eebb49a5e7a8d4ca446d1fc946447d07d08
jayesh <jayesh.bansod@iqvia.com>
2023-02-10 20:04:47 +0530

    fixed customDropdown

--- src/features/Components/CustomDropdown/CustomDropdown.js
+++ src/features/Components/CustomDropdown/CustomDropdown.js
@@ -77,22 +77,22 @@ function CustomDropdown({
     const substring = customListTemp.filter((item) => {
       return item.label.toLowerCase().match(subStr);
     });
-    feildChangeFun(substring, SetSubStringExist);
+    if (substring.length === 0) {
+      SetSubStringExist(true);
+    } else {
+      SetSubStringExist(false);
+    }
     const tempvalue = {
       label: e.target.value,
     };
     setValue(tempvalue);
     setList(filteredList);
-    targetValueFun(
-      e,
-      onChange,
-      fieldName,
-      fieldType,
-      tempvalue,
-      setBlur,
-      setList,
-      source,
-    );
+  if (e.target.value === '') {
+    // setting onblur to true when text is cut,so that error should display as field required
+    onChange(fieldName, e, fieldType, tempvalue);
+    setBlur(true);
+    setList(source);
+  }
   };
 
   const handleOutsideClick = (event) => {
