8a723bd1a986cba872a2c459d256833b66f12fbf
Muraleedharan <anil.kumar3@iqvia.com>
2023-02-17 15:17:08 +0530

    digitize accordion test cases

--- src/features/Container/Protocols/__test__/DigitizeAccordion.test.js
+++ src/features/Container/Protocols/__test__/DigitizeAccordion.test.js
@@ -1,10 +1,13 @@
 import { Provider } from 'react-redux';
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import store from '../../../../store/store';
 
 import { render, fireEvent } from '../../../../test-utils/test-utils';
 import DigitizeAccordion from '../DigitizedPanel/DigitizeAccordion';
+import ProtocolReducer from '../protocolSlice';
 import * as ProtocolContext from '../ProtocolContext';
+
+import initialState from './ProtocolReducer.json';
 
 const item = {
   doc_id: '558a1964-bfed-4974-a52b-79848e1df372',
@@ -24,6 +27,30 @@ const item = {
     total_no_review: '',
   },
 };
+
+// eslint-disable-next-line
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    store = configureStore({
+      reducer: { protocol: ProtocolReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {},
+) {
+  // eslint-disable-next-line
+  function Wrapper({ children }) {
+    const contextValues = { dispatchSectionEvent: jest.fn() };
+    jest
+      .spyOn(ProtocolContext, 'useProtContext')
+      .mockImplementation(() => contextValues);
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
 
 describe('DigitizeAccordion', () => {
   const bool = true;
@@ -189,5 +216,80 @@ describe('DigitizeAccordion', () => {
         AutoSizer: ({ children }) => children({ height: 1000, width: 1000 }),
       };
     });
+  });
+
+  test('Digitized accordion normal view', () => {
+    const item = {
+      doc_id: '78808eb2-6b1b-445f-bc89-4560ca66dd1c',
+      group_type: 'DocumentLinks',
+      link_id: 'bc4dc374-8a78-11ed-af64-005056ab6469',
+      LinkLevel: 1,
+      page: 1,
+      sec_id: '',
+      source_file_section: 'Signatures',
+      LinkType: 'toc',
+      qc_change_type: '',
+      sequence: 0,
+      section_locked: false,
+      audit_info: {
+        last_reviewed_date: '',
+        last_reviewed_by: '',
+        total_no_review: '',
+      },
+    };
+    const screen = renderWithProviders(
+      <DigitizeAccordion
+        item={item}
+        primaryRole={bool}
+        protocol="1234"
+        currentActiveCard="8ccb22b1-0aa0-487a-a47b"
+        setCurrentActiveCard={jest.fn()}
+        setCurrentEditCard={() => jest.fn()}
+      />,
+      {
+        preloadedState: initialState,
+      },
+    );
+    const enrText = screen.getByText('CONFIDENTIAL');
+    fireEvent.click(enrText);
+    screen.debug();
+  });
+
+  test('Digitized accordion clinical term view', () => {
+    const item = {
+      doc_id: '78808eb2-6b1b-445f-bc89-4560ca66dd1c',
+      group_type: 'DocumentLinks',
+      link_id: 'bc4dc374-8a78-11ed-af64-005056ab6469',
+      LinkLevel: 1,
+      page: 1,
+      sec_id: '',
+      source_file_section: 'Signatures',
+      LinkType: 'toc',
+      qc_change_type: '',
+      sequence: 0,
+      section_locked: false,
+      audit_info: {
+        last_reviewed_date: '',
+        last_reviewed_by: '',
+        total_no_review: '',
+      },
+    };
+    const screen = renderWithProviders(
+      <DigitizeAccordion
+        item={item}
+        primaryRole={bool}
+        protocol="1234"
+        currentActiveCard="8ccb22b1-0aa0-487a-a47b"
+        setCurrentActiveCard={jest.fn()}
+        setCurrentEditCard={() => jest.fn()}
+        rightBladeValue="Clinical Terms"
+      />,
+      {
+        preloadedState: initialState,
+      },
+    );
+    const enrText = screen.getByText('CONFIDENTIAL');
+    fireEvent.click(enrText);
+    screen.debug();
   });
 });
