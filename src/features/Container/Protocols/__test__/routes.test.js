d614328a28cca8ddab8b225310331a78a0f00536
Gurpreet Singh <gurpreet.singh@iqvia.com>
2023-02-18 08:45:03 +0530

    sonar test fixes

--- src/features/Container/Protocols/__test__/routes.test.js
+++ src/features/Container/Protocols/__test__/routes.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../../store/store';
-import Routes from '../routes';
+import store from '../../../../store/store';
+import Routes from '../../../../Routes/routes';
 
 describe('Routes component', () => {
   test('renders dashboard component for normal user', async () => {
