7a520a11eec96bdf3a937fc94c82059cb2814829
jayesh <jayesh.bansod@iqvia.com>
2023-02-20 12:07:12 +0530

    working on 4256 tickets

--- src/features/Container/Navbar/Alerts.js
+++ src/features/Container/Navbar/Alerts.js
@@ -1,19 +1,25 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import moment from 'moment';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import BellIcon from 'apollo-react-icons/Bell';
 import Badge from 'apollo-react/components/Badge';
 
 import Popover from 'apollo-react/components/Popover';
 import Typography from 'apollo-react/components/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from 'react-virtualized';
 import Tooltip from 'apollo-react/components/Tooltip';
-import { navbarNotifications } from './navbarSlice';
+import data from './__test__/alertdata.json';
 
 import './Alerts.scss';
 import { redaction } from '../../../AppConstant/AppConstant';
+import TrashIcon from 'apollo-react-icons/Trash';
+import IconButton from 'apollo-react/components/IconButton';
 
 const replaceall = require('replaceall');
 
@@ -35,8 +41,19 @@ function createFullMarkup(str) {
 function Alerts() {
   const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const notificationsMenuProps = useSelector(navbarNotifications);
-  if (!notificationsMenuProps.length) {
+  const notificationsMenuProps = data;
+  const [notificationsMenuPropS, setNotificationsMenuPropS] = useState(
+    notificationsMenuProps,
+  );
+
+  const cache = useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    }),
+  );
+
+  if (!notificationsMenuPropS.length) {
     return (
       <button
         data-testid="alert-bell-icon"
@@ -49,9 +66,16 @@ function Alerts() {
       </button>
     );
   }
+
+  const onDelete = (event, id) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const result = notificationsMenuPropS?.filter((item) => item?.id !== id);
+    setNotificationsMenuPropS(result);
+  };
+
   const checkForPrimary = async (data) => {
     dispatch({ type: 'READ_NOTIFICATION_SAGA', payload: data });
-
     // ---- Remove in local-----------
     // const axiosResp = await axios.get("/session");
     // const axiosUser = axiosResp.data;
@@ -60,7 +84,7 @@ function Alerts() {
     // ------Uncomment in Local -------
   };
 
-  const newNotifications = notificationsMenuProps.filter(
+  const newNotifications = notificationsMenuPropS.filter(
     (item) => item.read === false,
   );
   const getDayLabelText = (timestamp) => {
@@ -92,6 +116,8 @@ function Alerts() {
             borderColor: '#e9e9e9',
             boxShadow: '0 8px 20px 0 rgba(0, 0, 0, 0.08)',
             padding: 0,
+            height: '300px',
+            overflow: 'visible !important',
           },
         }}
         anchorOrigin={{
@@ -103,85 +129,120 @@ function Alerts() {
           horizontal: 'right',
         }}
       >
-        <List
-          data-testid="nav-alert-list"
-          className="root"
-          component="nav"
-          aria-label="main mailbox folders"
-        >
-          {notificationsMenuProps.map((item, i) => {
-            let header = null;
-            if (
-              i === 0 ||
-              !moment(notificationsMenuProps[i - 1].timestamp).isSame(
-                item.timestamp,
-                'day',
-              )
-            ) {
-              header = getDayLabelText(item.timestamp);
-            }
-            return (
-              <span key={item.id}>
-                {header && (
-                  <ListItem className="dayLabel">
-                    <Typography
-                      variant="body2"
-                      gutterBottom
-                      className="dayLabelText"
-                    >
-                      {header}
-                    </Typography>
-                  </ListItem>
-                )}
-
-                <div
-                  className={`ButtonBase-root ListItem-root 
+        <AutoSizer>
+          {({
+            width,
+            maxWidth,
+            borderColor,
+            boxShadow,
+            padding,
+            height,
+            overflow,
+          }) => (
+            <List
+              width={width}
+              maxWidth={maxWidth}
+              borderColor={borderColor}
+              boxShadow={boxShadow}
+              padding={padding}
+              height={height}
+              overflow={overflow}
+              rowHeight={cache.current.rowHeight}
+              deferredMeasurementCache={cache.current}
+              rowCount={notificationsMenuPropS.length}
+              rowRenderer={({ key, index, style, parent }) => {
+                const notification = notificationsMenuPropS[index];
+                return (
+                  <CellMeasurer
+                    key={key}
+                    cache={cache.current}
+                    parent={parent}
+                    rowIndex={index}
+                  >
+                    <div style={style}>
+                      <Typography className="notificationHeading">
+                        {'Notification'}
+                      </Typography>
+                      {[notification]?.map((item, i) => {
+                        let header = null;
+                        if (
+                          i === 0 ||
+                          !moment(notification[i - 1]?.timestamp).isSame(
+                            item.timestamp,
+                            'day',
+                          )
+                        ) {
+                          header = getDayLabelText(item.timestamp);
+                        }
+                        return (
+                          <span key={item.id}>
+                            <div
+                              className={`ButtonBase-root ListItem-root 
                     ${item.read ? 'listItem' : 'listItemNotRead'}
                    ListItem-button`}
-                  role="button"
-                  key={`${item.id}-${item.timestamp}`}
-                  onClick={() => checkForPrimary(item)}
-                >
-                  <div className="ListItemText-root listItemTextRoot ListItemText-multiline">
-                    {item.header && item.header.length > 30 ? (
-                      <Tooltip title={item.header} placement="top">
-                        <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
-                          {item.header}
-                        </span>
-                      </Tooltip>
-                    ) : (
-                      <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
-                        {item.header}
-                      </span>
-                    )}
-                    <Tooltip
-                      title="Protocol Title"
-                      subtitle={
-                        <div
-                          dangerouslySetInnerHTML={createFullMarkup(
-                            item.details,
-                          )}
-                        />
-                      }
-                      placement="right"
-                    >
-                      <p
-                        className="Typography-root ListItemText-secondary listItemTextSecondary Typography-body2 Typography-colorTextSecondary Typography-displayBlock"
-                        dangerouslySetInnerHTML={createFullMarkup(item.details)}
-                      />
-                    </Tooltip>
-                  </div>
+                              role="button"
+                              key={`${item.id}-${item.timestamp}`}
+                              onClick={() => checkForPrimary(item)}
+                            >
+                              <div className="ListItemText-root listItemTextRoot ListItemText-multiline">
+                                {item.header && item.header.length > 30 ? (
+                                  <Tooltip title={item.header} placement="top">
+                                    <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
+                                      {item.header}
+                                    </span>
+                                  </Tooltip>
+                                ) : (
+                                  <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
+                                    {item.header}
+                                  </span>
+                                )}
+                                <Tooltip
+                                  title="Protocol Title"
+                                  subtitle={
+                                    <div
+                                      dangerouslySetInnerHTML={createFullMarkup(
+                                        item.details,
+                                      )}
+                                    />
+                                  }
+                                  placement="right"
+                                >
+                                  <p
+                                    className="Typography-root ListItemText-secondary listItemTextSecondary Typography-body2 Typography-colorTextSecondary Typography-displayBlock"
+                                    dangerouslySetInnerHTML={createFullMarkup(
+                                      item.details,
+                                    )}
+                                  />
+                                </Tooltip>
+                                <hr className="horizontal-line" />
+                              </div>
 
-                  <p className="Typography-root timestamp Typography-body2 Typography-gutterBottom">
-                    {moment(item.timestamp).format('LT')}
-                  </p>
+                              <div className="IconButton-delete">
+                                <IconButton
+                                  size="small"
+                                  destructiveAction
+                                  onClick={(e) => onDelete(e, item.id)}
+                                >
+                                  <TrashIcon />
+                                </IconButton>
+                              </div>
+                              <p className="Typography-root timestamp Typography-body2 Typography-gutterBottom">
+                                {/* {moment(item.timestamp).format(' MMM D')} */}
+                                {header && <div>{header}</div>}
+                              </p>
 
-                  <span className="TouchRipple-root" />
-                </div>
-              </span>
-            );
-          })}
-        </List>
+                              <span className="TouchRipple-root" />
+                            </div>
+                          </span>
+                        );
+                      })}
+                    </div>
+                  </CellMeasurer>
+                );
+              }}
+            />
+          )}
+        </AutoSizer>
       </Popover>
     </>
   );
--- src/features/Container/Navbar/saga.js
+++ src/features/Container/Navbar/saga.js
@@ -49,13 +49,57 @@ export function* readNotification(action) {
   try {
     const readResp = yield call(httpCall, readConfig);
     if (readResp.success) {
-      window.location.href = `/protocols?protocolId=${data.aidocId}&tab=2`;
+      window.location.href = `/protocols?protocolId=${data.aidocId}&tab=1`;
     }
   } catch (err) {
     /* istanbul ignore next */
     console.log(err);
   }
 }
+
+// export function* deleteAttribute(action) {
+//   const {
+//     payload: { op, docId, fieldName, attributeNames, reqData },
+//   } = action;
+//   const config = {
+//     url: `${BASE_URL}${Apis.METADATA}/delete_meta_data`,
+//     method: 'DELETE',
+//     data: {
+//       id: data.id,
+//       protocol: data.protocol,
+//       aidocId: data.aidocId,
+//       readFlag: true,
+//     },
+//   };
+//   const data = yield call(httpCall, config);
+//   if (data?.data?.isDeleted) {
+//     if (op === 'deleteField') {
+//       yield put(
+//         getMetadataApiCall({
+//           status: true,
+//           reqData,
+//           op,
+//         }),
+//       );
+//       toast.info(`${reqData.name} successfully deleted`);
+//     } else {
+//       toast.info('attributes successfully deleted');
+//     }
+//   } else if (!data.success) {
+//     if (op === 'deleteField') {
+//       yield put(
+//         getMetadataApiCall({
+//           status: false,
+//           reqData,
+//           op,
+//         }),
+//       );
+//       toast.info(`${reqData.name} not deleted`);
+//     } else {
+//       toast.info('attributes not deleted');
+//     }
+//   }
+// }
 
 // export function* setRead(action) {
 //   const notificationUrl = `${BASE_URL_8000}/api/notification_read`;
