0443209a13fdd4146f0104333e12547c3e1a77bf
Muraleedharan <anil.kumar3@iqvia.com>
2023-02-23 15:42:53 +0530

    BladeRight.js unit testing

--- src/features/Container/Protocols/__test__/BladeRight.test.js
+++ src/features/Container/Protocols/__test__/BladeRight.test.js
@@ -1,4 +1,4 @@
-import { render } from '../../../../test-utils/test-utils';
+import { fireEvent, render } from '../../../../test-utils/test-utils';
 import BladeRight from '../BladeRight/BladeRight';
 
 const sample = [
@@ -10,11 +10,22 @@ const sample = [
 describe('Metadata Accordian View', () => {
   test('should render the component', () => {
     const component = render(<BladeRight data={sample} />);
-
     const metadataAccordian = component.getByTestId('rightblade');
-
     expect(component).toBeTruthy();
-
     expect(metadataAccordian).toBeInTheDocument();
+    const svg =
+      component.getByTestId('rightblade').children[0].children[0].children[0]
+        .children[0];
+    expect(svg).toBeInTheDocument();
+    fireEvent.click(svg);
+
+    const preferredTermSwitch = component.getByTestId('preferred-term-switch');
+    const input = preferredTermSwitch.children[0].children[0].children[0];
+    fireEvent.click(input);
+
+    const home = component.getByText('Home');
+    fireEvent.click(home);
+
+    component.debug();
   });
 });
