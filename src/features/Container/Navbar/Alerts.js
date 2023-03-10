0ca152a4c791e53f0e33dce1fdd7363c928ddb7a
jayesh <jayesh.bansod@iqvia.com>
2023-03-10 12:48:40 +0530

    completed all work

--- src/features/Container/Navbar/Alerts.js
+++ src/features/Container/Navbar/Alerts.js
@@ -83,7 +83,6 @@ function Alerts() {
   const newNotifications = notificationsMenuPropS.filter(
     (item) => item.read === false,
   );
-  
   const getDayLabelText = (timestamp) => {
     return moment().isSame(timestamp, 'day')
       ? 'Today'
@@ -102,157 +101,169 @@ function Alerts() {
           <BellIcon />
         </Badge>
       </button>
-
-      <Popover
-        open={!!anchorEl}
-        anchorEl={anchorEl}
-        onClose={() => setAnchorEl(null)}
-        PaperProps={{
-          style: {
-            width: '100%',
-            maxWidth: 360,
-            borderColor: '#e9e9e9',
-            boxShadow: '0 8px 20px 0 rgba(0, 0, 0, 0.08)',
-            padding: 0,
-            height: '360px',
-            overflow: 'visible !important',
-          },
-        }}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-      >
-        <h4 className="Heading-main">Notification</h4>
-        <AutoSizer>
-          {({
-            width,
-            maxWidth,
-            borderColor,
-            boxShadow,
-            padding,
-            height,
-            overflow,
-          }) => (
-            <List
-              width={width}
-              maxWidth={maxWidth}
-              borderColor={borderColor}
-              boxShadow={boxShadow}
-              padding={padding}
-              height={height}
-              overflow={overflow}
-              rowHeight={cache.current.rowHeight}
-              deferredMeasurementCache={cache.current}
-              rowCount={notificationsMenuPropS.length}
-              rowRenderer={({ key, index, style, parent }) => {
-                const notification = notificationsMenuPropS[index];
-                return (
-                  <CellMeasurer
-                    key={key}
-                    cache={cache.current}
-                    parent={parent}
-                    rowIndex={index}
-                  >
-                    <div style={style}>
-                      {[notification]?.map((item, i) => {
-                        let header = null;
-                        if (
-                          i === 0 ||
-                          !moment(notification[i - 1]?.timestamp).isSame(
-                            item.timestamp,
-                            'day',
-                          )
-                        ) {
-                          header = getDayLabelText(item.timestamp);
-                        }
-                        return (
-                          <span key={item.id}>
-                            <div
-                              className={`ButtonBase-root ListItem-root 
+      <div data-testid="showPopover">
+        {' '}
+        <Popover
+          open={!!anchorEl}
+          anchorEl={anchorEl}
+          data-testId="popover"
+          onClose={() => setAnchorEl(null)}
+          PaperProps={{
+            style: {
+              width: '100%',
+              maxWidth: 360,
+              borderColor: '#e9e9e9',
+              boxShadow: '0 8px 20px 0 rgba(0, 0, 0, 0.08)',
+              padding: 0,
+              height: '400px',
+              overflow: 'visible !important',
+            },
+          }}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <h4 className="Heading-main" data-testid="heading">
+            Notification
+          </h4>
+          <AutoSizer data-testId="list-item">
+            {({
+              width,
+              maxWidth,
+              borderColor,
+              boxShadow,
+              padding,
+              height,
+              overflow,
+            }) => (
+              <List
+                data-testId="list-item-2"
+                width={width}
+                maxWidth={maxWidth}
+                borderColor={borderColor}
+                boxShadow={boxShadow}
+                padding={padding}
+                height={height}
+                overflow={overflow}
+                rowHeight={cache.current.rowHeight}
+                deferredMeasurementCache={cache.current}
+                rowCount={notificationsMenuPropS.length}
+                rowRenderer={({ key, index, style, parent }) => {
+                  const notification = notificationsMenuPropS[index];
+                  return (
+                    <CellMeasurer
+                      key={key}
+                      cache={cache.current}
+                      parent={parent}
+                      rowIndex={index}
+                      data-testId="cell-measurer"
+                    >
+                      <div style={style}>
+                        {[notification]?.map((item, i) => {
+                          let header = null;
+                          if (
+                            i === 0 ||
+                            !moment(notification[i - 1]?.timestamp).isSame(
+                              item.timestamp,
+                              'day',
+                            )
+                          ) {
+                            header = getDayLabelText(item.timestamp);
+                          }
+                          return (
+                            <span key={item.id}>
+                              <div
+                                data-testId="list-Item"
+                                className={`ButtonBase-root ListItem-root 
                     ${item.read ? 'listItem' : 'listItemNotRead'}
                    ListItem-button`}
-                              role="button"
-                              key={`${item.id}-${item.timestamp}`}
-                              onClick={() =>
-                                checkForPrimary(
-                                  item.aidocId,
-                                  item.id,
-                                  item.protocol,
-                                )
-                              }
-                            >
-                              <div className="ListItemText-root listItemTextRoot ListItemText-multiline">
-                                {item.header && item.header.length > 30 ? (
-                                  <Tooltip title={item.header} placement="top">
+                                role="button"
+                                key={`${item.id}-${item.timestamp}`}
+                                onClick={() =>
+                                  checkForPrimary(
+                                    item.aidocId,
+                                    item.id,
+                                    item.protocol,
+                                  )
+                                }
+                              >
+                                <div className="ListItemText-root listItemTextRoot ListItemText-multiline">
+                                  {item.header && item.header.length > 30 ? (
+                                    <Tooltip
+                                      title={item.header}
+                                      placement="top"
+                                    >
+                                      <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
+                                        {item.header}
+                                      </span>
+                                    </Tooltip>
+                                  ) : (
                                     <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
                                       {item.header}
                                     </span>
-                                  </Tooltip>
-                                ) : (
-                                  <span className="Typography-root ListItemText-primary listItemTextPrimary Typography-body1 Typography-displayBlock">
-                                    {item.header}
-                                  </span>
-                                )}
-                                <Tooltip
-                                  title="Protocol Title"
-                                  subtitle={
-                                    <div
+                                  )}
+                                  <Tooltip
+                                    title="Protocol Title"
+                                    subtitle={
+                                      <div
+                                        dangerouslySetInnerHTML={createFullMarkup(
+                                          item.details,
+                                        )}
+                                      />
+                                    }
+                                    placement="right"
+                                  >
+                                    <p
+                                      className="Typography-root ListItemText-secondary listItemTextSecondary Typography-body2 Typography-colorTextSecondary Typography-displayBlock"
                                       dangerouslySetInnerHTML={createFullMarkup(
                                         item.details,
                                       )}
                                     />
-                                  }
-                                  placement="right"
-                                >
-                                  <p
-                                    className="Typography-root ListItemText-secondary listItemTextSecondary Typography-body2 Typography-colorTextSecondary Typography-displayBlock"
-                                    dangerouslySetInnerHTML={createFullMarkup(
-                                      item.details,
-                                    )}
-                                  />
-                                </Tooltip>
-                                <hr className="horizontal-line" />
-                              </div>
+                                  </Tooltip>
+                                  <hr className="horizontal-line" />
+                                </div>
 
-                              <div className="IconButton-delete">
-                                <IconButton
-                                  size="small"
-                                  destructiveAction
-                                  onClick={(e) =>
-                                    onDelete(
-                                      e,
-                                      item.aidocId,
-                                      item.id,
-                                      item.protocol,
-                                    )
-                                  }
-                                >
-                                  <TrashIcon />
-                                </IconButton>
-                              </div>
-                              <p className="Typography-root timestamp Typography-body2 Typography-gutterBottom">
-                                {/* {moment(item.timestamp).format(' MMM D')} */}
-                                {header && <div>{header}</div>}
-                              </p>
+                                <div className="IconButton-delete">
+                                  <IconButton
+                                    data-testId="trash-icon-1"
+                                    size="small"
+                                    destructiveAction
+                                    onClick={(e) =>
+                                      onDelete(
+                                        e,
+                                        item.aidocId,
+                                        item.id,
+                                        item.protocol,
+                                      )
+                                    }
+                                  >
+                                    <TrashIcon />
+                                  </IconButton>
+                                </div>
+                                <p className="Typography-root timestamp Typography-body2 Typography-gutterBottom">
+                                  {/* {moment(item.timestamp).format(' MMM D')} */}
+                                  {header && <div>{header}</div>}
+                                </p>
 
-                              <span className="TouchRipple-root" />
-                            </div>
-                          </span>
-                        );
-                      })}
-                    </div>
-                  </CellMeasurer>
-                );
-              }}
-            />
-          )}
-        </AutoSizer>
-      </Popover>
+                                <span className="TouchRipple-root" />
+                              </div>
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </CellMeasurer>
+                  );
+                }}
+              />
+            )}
+          </AutoSizer>
+        </Popover>
+      </div>
     </>
   );
 }
