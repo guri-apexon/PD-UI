67533f38328c2ef330d23c3542211d647a004426
Shipra Vishnoi <shipra.vishnoi@1qvia.com>
2023-03-16 12:20:32 +0530

    edit feature added

--- src/features/Container/Protocols/LabData/LabData.js
+++ src/features/Container/Protocols/LabData/LabData.js
@@ -1,11 +1,116 @@
 import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Card from 'apollo-react/components/Card/Card';
-import Table from 'apollo-react/components/Table';
+import TextField from 'apollo-react/components/TextField';
+import Filter from 'apollo-react-icons/Filter';
+import Save from 'apollo-react-icons/Save';
+import Pencil from 'apollo-react-icons/Pencil';
+import EllipsisVertical from 'apollo-react-icons/EllipsisVertical';
+import Table, { createStringSearchFilter } from 'apollo-react/components/Table';
+import Button from 'apollo-react/components/Button';
+import IconMenuButton from 'apollo-react/components/IconMenuButton';
 import LABDATA_CONSTANTS from './constants';
+import './LabData.scss';
+
+function TextFieldFilter() {
+  return (
+    <TextField
+      // value={filters[accessor]}
+      // name={accessor}
+      // onChange={updateFilterValue}
+      fullWidth
+      margin="none"
+      size="small"
+    />
+  );
+}
+
+function ActionCell({ row }) {
+  const { id, onRowEdit, onDelete } = row;
+  const menuItems = [
+    {
+      text: 'Edit',
+      onClick: () => onRowEdit(id),
+    },
+    {
+      text: 'Delete',
+      onClick: () => onDelete(id),
+    },
+  ];
+  return (
+    <div>
+      <IconMenuButton menuItems={menuItems}>
+        <EllipsisVertical className="ellipsis-icon" />
+      </IconMenuButton>
+    </div>
+  );
+}
+
+function EditableCell({ row, column: { accessor: key } }) {
+  const { editMode } = row;
+  return editMode ? (
+    <TextField
+      size="small"
+      fullWidth
+      value={row[key]}
+      onChange={(e) => row.handleChange(key, e.target.value)}
+    />
+  ) : (
+    row[key]
+  );
+}
 
 function LabData() {
-  const rows = [];
+  const [columns, setColumns] = useState(LABDATA_CONSTANTS.columnList);
+  const [rowData, setRowData] = useState(LABDATA_CONSTANTS.records);
+  const [editedRow, setEditedRow] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleSave = () => {
+    setIsEdit(false);
+    setColumns(columns.filter((col) => col.accessor !== 'menu'));
+  };
+
+  const onRowEdit = (id) => {
+    setEditedRow(rowData.find((row) => row.id === id));
+  };
+
+  const onDelete = () => {};
+
+  const handleChange = (key, value) => {
+    setEditedRow({ ...editedRow, [key]: value });
+  };
+
+  useEffect(() => {
+    setColumns(
+      columns.map((col) => {
+        const isIncluded = ['id', 'menu'].includes(col.accessor);
+        if (!isIncluded) {
+          return {
+            ...col,
+            customCell: EditableCell,
+          };
+        }
+        return col;
+      }),
+    );
+  }, []);
+
+  useEffect(() => {
+    if (isEdit) {
+      setColumns([
+        ...columns,
+        {
+          header: '',
+          accessor: 'menu',
+          customCell: ActionCell,
+        },
+      ]);
+    }
+    // eslint-disable-next-line
+  }, [isEdit]);
+
+  // console.log('columns--->', columns);
   return (
     <Card
       className="protocol-column protocol-digitize-column metadata-card"
@@ -14,10 +119,37 @@ function LabData() {
       <div className="panel-heading ">
         <div className="metadat-flex-plus"> Lab Data </div>
       </div>
-      <div>
+      <div className="lab-btn-container">
+        <Button variant="secondary" icon={<Filter />} className="btn-filter mR">
+          Filters
+        </Button>
+        {isEdit ? (
+          <Button
+            variant="secondary"
+            icon={<Save />}
+            className="btn-common"
+            onClick={handleSave}
+          />
+        ) : (
+          <Button
+            variant="secondary"
+            icon={<Pencil />}
+            className="btn-common"
+            onClick={() => setIsEdit(true)}
+          />
+        )}
+      </div>
+      <div className="lab-table-container">
         <Table
-          columns={LABDATA_CONSTANTS.columnList}
-          rows={rows}
+          columns={columns}
+          rows={rowData?.map((row) => ({
+            ...row,
+            editMode: editedRow.id === row.id,
+            editedRow,
+            onRowEdit,
+            onDelete,
+            handleChange,
+          }))}
           rowId="id"
           hidePagination
         />
@@ -25,5 +157,16 @@ function LabData() {
     </Card>
   );
 }
+
+TextFieldFilter.propTypes = {};
+
+ActionCell.propTypes = {
+  row: PropTypes.isRequired,
+};
+
+EditableCell.propTypes = {
+  row: PropTypes.isRequired,
+  column: PropTypes.isRequired,
+};
 
 export default LabData;
--- src/features/Container/Protocols/LabData/constants.js
+++ src/features/Container/Protocols/LabData/constants.js
@@ -25,4 +25,46 @@ const columnList = [
   },
 ];
 
-export default { columnList };
+const records = [
+  {
+    id: 'Table 1',
+    name: 'Laboratory Safety Variables',
+    assName: 'Haematology',
+    prName: 'B-Haemoglobin',
+    assPrefName: 'Haematology',
+    prPrefName: 'NA',
+  },
+  {
+    id: 'Table 2',
+    name: 'Laboratory Safety Variables',
+    assName: 'Haematology',
+    prName: 'B-Leukocyte',
+    assPrefName: 'Haematology',
+    prPrefName: 'NA',
+  },
+  {
+    id: 'Table 3',
+    name: 'Laboratory Safety Variables',
+    assName: 'Haematology',
+    prName: 'Neutrophils',
+    assPrefName: 'Haematology',
+    prPrefName: 'NA',
+  },
+  {
+    id: 'Table 4',
+    name: 'Laboratory Safety Variables',
+    assName: 'Haematology',
+    prName: 'Lymphocytes',
+    assPrefName: 'Haematology',
+    prPrefName: 'NA',
+  },
+  {
+    id: 'Table 5',
+    name: 'Laboratory Safety Variables',
+    assName: 'Haematology',
+    prName: 'U-Glucose',
+    assPrefName: 'Haematology',
+    prPrefName: 'NA',
+  },
+];
+export default { columnList, records };
