90a6396ad4896767ae93f82240bb43eda8c5479f
Patil, Nilesh EX1 <nilesh.patil@quintiles.com>
2023-03-16 08:34:02 +0000

    Dipa View Integration with mock data

---
+++ src/features/Container/Protocols/DipaView/Data.js
@@ -0,0 +1,105 @@
+const data = [
+  {
+    doc_id: '',
+    header: '',
+    subheader: '',
+    Actual_count: '',
+    Total_derive_count: '',
+    output: [
+      {
+        ID: '1',
+        actual_text:
+          'For inclusion in the study, a patient must meet and comply with the following criteri',
+        level: '0',
+        child: [
+          {
+            ID: '2',
+            actual_text:
+              '1. Patient has successfully completed the Maintenance and Transition phases of Study VRX-RET-E22-302 for the treatment of partial-onset seizures',
+            level: '1',
+            derive_seg: [
+              {
+                ID: '2.1',
+                derive_seg:
+                  'Patient has successfully completed the Maintenance and Transition phases of Study VRX-RET-E22-302 for the treatment of partial-onset seizures',
+              },
+            ],
+            child: [],
+          },
+          {
+            ID: '3',
+            actual_text:
+              '2. Patient is expected to benefit from participation in the study in the opinion of the Investigator.',
+            level: '1',
+            derive_seg: [
+              {
+                ID: '3.1',
+                derive_seg:
+                  'Patient is expected to benefit from participation in the study in the opinion of the Investigator.',
+              },
+            ],
+            child: [],
+          },
+          {
+            ID: '4',
+            actual_text:
+              '3. Women of childbearing potential and fertile males have to agree to use a medically acceptable method of birth control and females had to have a negative serum \u03b2-HCG pregnancy test at the last visit of the Transition phase of Study VRX-RET-E22-302. A woman of childbearing potential is defined as a female who is biologically capable of becoming pregnant. A medically acceptable method of birth control includes an intrauterine device in place for at least 3 months, surgical sterilization (e.g. tuballigation), or adequate barrier methods (e.g., diaphragm and foam). An oral contraceptivealone is not considered adequate for the purpose of this study.',
+            level: '1',
+            derive_seg: [
+              {
+                ID: '4.1',
+                derive_seg:
+                  'Women of childbearing potential and fertile males have to agree to use a medically acceptable method of birth control and females had to have a negative serum \u03b2-HCG pregnancy test at the last visit of the Transition phase of Study VRX-RET-E22-302.',
+              },
+              {
+                ID: '4.2',
+                derive_seg:
+                  'A woman of childbearing potential is defined as a female who is biologically capable of becoming pregnant.',
+              },
+              {
+                ID: '4.3',
+                derive_seg:
+                  'A medically acceptable method of birth control includes an intrauterine device in place for at least 3 months, surgical sterilization (e.g. tuballigation), or adequate barrier methods (e.g., diaphragm and foam).',
+              },
+              {
+                ID: '4.4',
+                derive_seg:
+                  'An oral contraceptivealone is not considered adequate for the purpose of this study.',
+              },
+            ],
+            child: [],
+          },
+          {
+            ID: '5',
+            actual_text:
+              '4. In the opinion of the Investigator, patient is able to understand verbal and written instructions and will adhere to all study schedules and requirements.',
+            level: '1',
+            derive_seg: [
+              {
+                ID: '5.1',
+                derive_seg:
+                  'In the opinion of the Investigator, patient is able to understand verbal and written instructions and will adhere to all study schedules and requirements.',
+              },
+            ],
+            child: [],
+          },
+          {
+            ID: '6',
+            actual_text:
+              '5. Patient or legal guardian (if applicable) is informed, given ample time and opportunity to read and/or understand about his/her participation in the study, and has signed and dated the written informed consent form.',
+            level: '1',
+            derive_seg: [
+              {
+                ID: '6.1',
+                derive_seg:
+                  'Patient or legal guardian (if applicable) is informed, given ample time and opportunity to read and/or understand about his/her participation in the study, and has signed and dated the written informed consent form.',
+              },
+            ],
+            child: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+export default data;
