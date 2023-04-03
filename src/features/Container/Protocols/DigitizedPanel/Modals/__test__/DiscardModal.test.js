265cb6ffdb70a4252584fc6abe6334b8d88fcfea
Anil Kumar Muraleedharan <anil.kumar3@iqvia.com>
2023-04-03 11:45:19 +0000

    Code refactor

---
+++ src/features/Container/Protocols/DigitizedPanel/Modals/__test__/DiscardModal.test.js
@@ -0,0 +1,71 @@
+import { render, fireEvent } from '@testing-library/react';
+import DiscardModal from '../DiscardModal';
+
+describe('Discard Modal', () => {
+  test('Discard Modal load', () => {
+    const showDiscardConfirm = true;
+    const setShowDiscardConfirm = jest.fn();
+    const onDiscardClick = jest.fn();
+    const component = render(
+      <DiscardModal
+        classes={{ modal: '' }}
+        currentEditCard="123"
+        showConfirm
+        showDiscardConfirm={showDiscardConfirm}
+        setShowDiscardConfirm={setShowDiscardConfirm}
+        onDiscardClick={onDiscardClick}
+      />,
+    );
+    expect(component).toBeTruthy();
+    expect(component.getByText('Cancel')).toBeInTheDocument();
+    expect(component.getByText('Discard')).toBeInTheDocument();
+  });
+
+  test('Cancel Button Click', () => {
+    const showDiscardConfirm = true;
+    const setShowDiscardConfirm = jest.fn();
+    const onDiscardClick = jest.fn();
+
+    const component = render(
+      <DiscardModal
+        classes={{ modal: '' }}
+        currentEditCard="123"
+        showConfirm
+        showDiscardConfirm={showDiscardConfirm}
+        setShowDiscardConfirm={setShowDiscardConfirm}
+        onDiscardClick={onDiscardClick}
+      />,
+    );
+
+    expect(component).toBeTruthy();
+    const cancelBtn = component.getByText('Cancel');
+    expect(cancelBtn).toBeInTheDocument();
+    fireEvent.click(cancelBtn);
+
+    expect(setShowDiscardConfirm).toHaveBeenCalled();
+  });
+
+  test('Discard Button Click', () => {
+    const showDiscardConfirm = true;
+    const setShowDiscardConfirm = jest.fn();
+    const onDiscardClick = jest.fn();
+
+    const component = render(
+      <DiscardModal
+        classes={{ modal: '' }}
+        currentEditCard="123"
+        showConfirm
+        showDiscardConfirm={showDiscardConfirm}
+        setShowDiscardConfirm={setShowDiscardConfirm}
+        onDiscardClick={onDiscardClick}
+      />,
+    );
+
+    expect(component).toBeTruthy();
+
+    const discardBtn = component.getByText('Discard');
+    expect(discardBtn).toBeInTheDocument();
+    fireEvent.click(discardBtn);
+    expect(onDiscardClick).toHaveBeenCalled();
+  });
+});
