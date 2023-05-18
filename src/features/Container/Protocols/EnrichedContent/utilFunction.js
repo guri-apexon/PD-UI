5e612344f5aebdf3571064ecad3fbb52bbac92b1
Gurpreet Singh <gurpreet.singh@iqvia.com>
2023-05-18 17:35:22 +0530

    msg fixes

--- src/features/Container/Protocols/EnrichedContent/utilFunction.js
+++ src/features/Container/Protocols/EnrichedContent/utilFunction.js
@@ -7,16 +7,14 @@ const getKeyFromEnrichText = (term) => {
 
 const preferredTermsValidation = (value, preferredTerm) => {
   let msg = '';
-  let extendMsg = '';
   const pTLowerCase = value.toLowerCase();
   const arr = value.split(' ');
   if (value === '') {
     msg = '';
   } else if (value.includes(',') || arr.length > 1 || preferredTerm) {
     msg = 'Please tag only one Preferred Term. ';
-    extendMsg = preferredTerm && `${preferredTerm} is already tagged Term`;
-    if (extendMsg) {
-      msg += extendMsg;
+    if (preferredTerm) {
+      msg += `${preferredTerm} is already tagged Term`;
     }
   } else if (!pTLowerCase.includes('cpt_')) {
     msg = 'Please prefix Preferred Term with "cpt_"';
