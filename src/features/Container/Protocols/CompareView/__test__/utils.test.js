e48fd29b4da8368188fbd9f7c0134cac928e60b3
Gurpreet Singh <gurpreet.singh@iqvia.com>
2023-07-14 18:08:58 +0530

    Revert "Draftable integration"
    This reverts commit 6827e041d76f5cabf315274c20301604e25c58f5.

--- src/features/Container/Protocols/CompareView/__test__/utils.test.js
+++
@@ -1,9 +0,0 @@
-import { blobToFormData } from '../utils';
-
-describe('blobToFormData', () => {
-  it('should create a FormData object with the correct values', () => {
-    const sourceBlob = new Blob(['Source File'], { type: 'application/pdf' });
-    const targetBlob = new Blob(['Target File'], { type: 'application/pdf' });
-    blobToFormData(sourceBlob, targetBlob);
-  });
-});
