896ddcabec0c38eb2618ad6d3166202dacfd27fe
Samal <subhadatta.samal@iqvia.com>
2023-08-23 18:55:53 +0530

    Assessment and Visit Accordion Edit click

--- src/features/Container/Protocols/MetaData/Visit/Visits.js
+++ src/features/Container/Protocols/MetaData/Visit/Visits.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Accordion from 'apollo-react/components/Accordion';
 import AccordionDetails from 'apollo-react/components/AccordionDetails';
@@ -7,6 +7,8 @@ import AccordionSummary from 'apollo-react/components/AccordionSummary';
 import Typography from 'apollo-react/components/Typography';
 import Pencil from 'apollo-react-icons/Pencil';
 import EyeShow from 'apollo-react-icons/EyeShow';
+import Save from 'apollo-react-icons/Save';
+import Undo from 'apollo-react-icons/Undo';
 import AssessmentVisitTable from '../Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { visitData } from '../../protocolSlice';
@@ -14,10 +16,25 @@ import { visitData } from '../../protocolSlice';
 const Visits = ({ docId }) => {
   const dispatch = useDispatch();
   const visitsData = useSelector(visitData);
-  console.log('assessments', visitsData);
+  const [isEditEnabled, setEditEnabled] = useState(false);
+
   useEffect(() => {
     dispatch({ type: 'GET_VISITS', payload: { docId } });
   }, []);
+
+  const handleSaveData = (e) => {
+    e.stopPropagation();
+    setEditEnabled(false);
+  };
+
+  const handleUndo = (e) => {
+    e.stopPropagation();
+  };
+  const handleEdit = (e) => {
+    e.stopPropagation();
+    setEditEnabled(true);
+  };
+
   return (
     <div>
       <Accordion>
@@ -28,12 +45,38 @@ const Visits = ({ docId }) => {
               <span data-testId="eyeIcon" role="presentation">
                 <EyeShow style={{ paddingRight: '10px' }} />
               </span>
-              <span data-testId="metadatapencil">
-                <Pencil
-                  className="metadata-plus-size"
-                  data-testid="handle-edit"
-                />
-              </span>
+              {!isEditEnabled ? (
+                <span data-testId="metadatapencil">
+                  <Pencil
+                    className="metadata-plus-size"
+                    data-testid="handle-edit"
+                    onClick={(e) => {
+                      handleEdit(e);
+                    }}
+                  />
+                </span>
+              ) : (
+                <>
+                  <span
+                    data-testId="metadatasave"
+                    onClick={(e) => {
+                      handleSaveData(e);
+                    }}
+                    role="presentation"
+                  >
+                    <Save className="metadata-plus-size mR" />
+                  </span>
+                  <span
+                    data-testId="metadatadiscard"
+                    onClick={(e) => {
+                      handleUndo(e);
+                    }}
+                    role="presentation"
+                  >
+                    <Undo className="metadata-plus-size" />
+                  </span>
+                </>
+              )}
             </div>
           </div>
         </AccordionSummary>
@@ -43,6 +86,7 @@ const Visits = ({ docId }) => {
               data={visitsData.data.data}
               columns={visitsData.data.columns}
               settings={true}
+              editEnabled={isEditEnabled}
             />
           )}
         </AccordionDetails>
